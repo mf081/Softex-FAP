603d915a41c09f8ca600d33f042bc756503de48f
mf081 <166130931+mf081@users.noreply.github.com>
2024-09-19 00:23:55 +0000

    fazer atividade entrada e manipulação de dados

---
+++ entrada-e-manipulação-de-dados/README.md
@@ -0,0 +1,66 @@
+5. Solicite ao usuário seu nome e imprima uma mensagem de boas-vindas na tela.
+6. Peça ao usuário que digite sua idade em texto (por exemplo, "18") e converta-a em um número
+inteiro.
+7. Receba um número inteiro do usuário e converta-o em um número decimal (float).
+8. Peça ao usuário para digitar dois números inteiros e exiba a soma deles.
+9. Receba um número decimal do usuário e calcule o seu quadrado.
+10. Peça ao usuário que insira o seu ano de nascimento e, em seguida, exiba a sua idade.
+11. Peça ao usuário que digite seu primeiro nome e seu sobrenome separadamente. Em seguida,
+concatene-os em uma única string e exiba o nome completo.
+12. Solicite ao usuário uma sequência de números separados por espaço e exiba quantos números
+foram digitados.
+13. Receba o nome de um animal digitado pelo usuário e exiba uma mensagem informando qual
+animal foi digitado.
+14. Peça ao usuário que digite o seu nome e o seu sobrenome. Em seguida, exiba o nome completo
+invertido (sobrenome, nome).
+15. Receba uma string digitada pelo usuário e imprima o seu tamanho (número de caracteres).
+16. Solicite ao usuário um número inteiro e exiba se ele é par ou ímpar.
+17. Receba um número inteiro digitado pelo usuário e verifique se ele é positivo ou negativo.
+18. Peça ao usuário que insira dois números e exiba o maior deles.
+
+19. Receba a altura e o peso de uma pessoa digitados pelo usuário. Em seguida, calcule o seu índice de
+massa corporal (IMC) utilizando a fórmula: IMC = peso / (altura * altura) e exiba o resultado.
+20. Peça ao usuário que digite o seu nome e verifique se ele contém mais de 5 caracteres.
+21. Solicite ao usuário que insira o seu estado civil e exiba uma mensagem apropriada (por exemplo:
+"Você é casado(a)", "Você é solteiro(a)", etc.).
+22. Receba a base e a altura de um retângulo digitados pelo usuário. Em seguida, calcule a sua área e
+exiba o resultado.
+23. Peça ao usuário que digite a sua cidade e verifique se ela começa com a letra "S" (ou outra letra de
+sua escolha).
+24. Solicite ao usuário que insira dois números decimais e calcule o resto da divisão entre eles.
+25. Solicite ao usuário um número decimal e converta-o em um número inteiro.
+26. Receba uma string contendo um número inteiro e some 10 a esse número, convertendo-o
+novamente para uma string antes de exibi-lo.
+27. Solicite ao usuário que digite uma data no formato "dd/mm/aaaa" e extraia o dia, o mês e o ano
+separadamente, convertendo-os em números inteiros.
+28. Receba o nome de uma cidade do usuário e concatene-o com o nome do estado para formar uma
+mensagem completa, como "Você mora em [cidade], [estado].".
+29. Solicite ao usuário que insira seu ano de nascimento e concatene-o com uma mensagem de
+boas-vindas, como "Bem-vindo ao nosso programa, nascido em [ano de nascimento]!".
+30. Receba um número inteiro e uma string do usuário. Em seguida, concatene-os em uma única
+string, separando-os com um espaço.
+31. Receba o nome de um produto digitado pelo usuário e concatene-o com o preço do produto,
+adicionando o símbolo de moeda da sua escolha.
+32. Receba um número inteiro do usuário e concatene-o com uma mensagem, informando o dobro
+desse número.
+33. Receba uma string contendo um endereço de e-mail e concatene-a com uma mensagem de
+agradecimento personalizada.
+34. Receba dois números inteiros do usuário e exiba a soma, a diferença, o produto e o quociente
+(divisão inteira) entre eles.
+35. Peça ao usuário para digitar a base e a altura de um triângulo. Em seguida, calcule e exiba a área do
+triângulo.
+36. Receba o raio de uma circunferência digitado pelo usuário e calcule o seu perímetro (2 * π * raio).
+
+37. Receba a base e a altura de um retângulo digitados pelo usuário. Em seguida, calcule e exiba o
+perímetro do retângulo.
+38. Solicite ao usuário que insira três números decimais. Em seguida, calcule e exiba a média
+aritmética desses números.
+39. Peça ao usuário para digitar a sua idade e, em seguida, informe quantos meses e quantos dias ele já
+viveu (considerando um ano com 365 dias).
+40. Receba um valor em reais e a cotação do dólar digitados pelo usuário. Em seguida, converta o valor
+para dólares e exiba o resultado.
+41. Solicite ao usuário para digitar um número decimal e arredonde-o para o inteiro mais próximo.
+42. Receba três números inteiros digitados pelo usuário e exiba o resultado da operação (n1 + n2) *
+n3.
+43. Peça ao usuário que digite uma temperatura em graus Celsius e a converta para Fahrenheit usando
+a fórmula: Fahrenheit = (Celsius * 9/5) + 32.
---
+++ entrada-e-manipulação-de-dados/atividade03.js
@@ -0,0 +1,195 @@
+// //1
+// const fraseUsuario = prompt("Entre a primeira frase");
+// console.log(fraseUsuario);
+// //2
+// const valorString = "123";
+// console.log(typeof Number(valorString));
+// const valorNumero = 321;
+// console.log(typeof String(valorNumero));
+// //3
+// const num1 = prompt("Digite o primeiro número");
+// const num2 = prompt("Digite o segundo número");
+// const soma = Number(num1) + Number(num2);
+// console.log(soma);
+// //4
+// const texto1: string = "Estudar";
+// const texto2: string = "é bom!";
+// const valorNum: number = 48;
+// console.log(texto1 + texto2 + " " + valorNum);
+// //5
+// const usuarioNome = prompt("Qual o seu nome?");
+// alert(`Boas-vindas ${usuarioNome}!`);
+// //6
+// const idadeUsuario = String(prompt("Digite sua idade:"));
+// console.log(Number(idadeUsuario));
+// //7
+// const numeroEntrada = String(prompt("Digite sua idade:"));
+// console.log(parseFloat(numeroEntrada));
+// //8
+// const primeiroNumero = prompt("Digite o primeiro número");
+// const segundoNumero = prompt("Digite o segundo número");
+// const somaNumeros = Number(primeiroNumero) + Number(segundoNumero);
+// //9
+// const numeroDecimalStr = String(prompt("Digite um número:"));
+// const numeroDecimal: number = parseFloat(numeroDecimalStr);
+// const resultadoQuadrado = numeroDecimal ** 2;
+// console.log(resultadoQuadrado);
+// //10
+// const anoNascimento = String(prompt("Digite seu ano de nascimento:"));
+// const idadeCalculada = 2024 - Number(anoNascimento);
+// console.log(`Você tem ou irá fazer esse ano: ${idadeCalculada}`);
+// //11
+// const primeiroNome = String(prompt("Digite seu nome:"));
+// const ultimoNome = String(prompt("Digite seu segundo nome:"));
+// const nomeCompleto = primeiroNome + " " + ultimoNome;
+// console.log(nomeCompleto);
+// //12
+// const numerosEntrada = String(prompt("Digite uma sequência de números separados por espaço:"));
+// let numerosSeparados = numerosEntrada.split(" ");
+// const quantidadeNumerosEntrada: number = numerosSeparados.length;
+// console.log(quantidadeNumerosEntrada);
+// //13
+// const nomeAnimal = prompt("Digite o nome de um animal:");
+// console.log(`O nome do animal digitado foi ${nomeAnimal}:`);
+// //14
+// const primeiroNomeT = prompt("Digite um nome:");
+// const ultimoNomeT = prompt("Digite um sobrenome:");
+// console.log(ultimoNomeT, primeiroNomeT);
+// //15
+// const entradaTexto = prompt("Digite um nome e direi o número de caracteres");
+// console.log(`${entradaTexto} tem ${entradaTexto?.length} caracteres`);
+// //16
+// const numeroParImpar = prompt('Digite um número:');
+// if (Number(numeroParImpar) % 2 == 0) {
+//     alert(`${numeroParImpar} é um número par.`);
+// } else {
+//     alert(`${numeroParImpar} é um número ímpar.`);
+// }
+// //17
+// const numeroPosNeg = prompt('Digite um número:');
+// if (Number(numeroPosNeg) > 0) {
+//     alert(`${numeroPosNeg} é um número positivo.`);
+// } else {
+//     alert(`${numeroPosNeg} é um número negativo.`);
+// }
+// //18
+// const primeiroNumeroComp = prompt('Digite o primeiro número:');
+// const segundoNumeroComp = prompt('Digite o segundo número:');
+// if (Number(primeiroNumeroComp) > Number(segundoNumeroComp)) {
+//     alert(`${primeiroNumeroComp} é maior.`);
+// } else {
+//     alert(`${segundoNumeroComp} é maior.`);
+// }
+// //19
+// const alturaPessoa = (prompt('Digite a altura (em metros):'));
+// const pesoPessoa = (prompt('Digite o peso (em quilogramas):'));
+// const imcCalculado = Number(pesoPessoa) / (Number(alturaPessoa) * Number(alturaPessoa));
+// alert(`O seu IMC é: ${(imcCalculado).toFixed(2)}`);
+// //20
+// const nomeDigitado = prompt('Digite o seu nome:');
+// if (String(nomeDigitado).length > 5) {
+//     alert(`O nome "${nomeDigitado}" possui mais de 5 caracteres.`);
+// } else {
+//     alert(`O nome "${nomeDigitado}" possui 5 caracteres ou menos.`);
+// }
+// //21
+// let estadoCivilUsuario = prompt('Digite o seu estado civil (casado, solteiro, divorciado, viúvo):');
+// estadoCivilUsuario = String(estadoCivilUsuario).toLowerCase().trim();
+// if (estadoCivilUsuario === 'casado') {
+//     alert('Você é casado(a).');
+// } else if (estadoCivilUsuario === 'solteiro') {
+//     alert('Você é solteiro(a).');
+// } else if (estadoCivilUsuario === 'divorciado') {
+//     alert('Você é divorciado(a).');
+// } else if (estadoCivilUsuario === 'viúvo') {
+//     alert('Você é viúvo(a).');
+// } else {
+//     alert('Estado civil não reconhecido.');
+// }
+// //22
+// const baseRetangulo = prompt('Digite a base do retângulo (em unidades):');
+// const alturaRetangulo = prompt('Digite a altura do retângulo (em unidades):');
+// const areaRetangulo = Number(baseRetangulo) * Number(alturaRetangulo);
+// alert(`A área do retângulo com base ${baseRetangulo} e altura ${alturaRetangulo} é: ${areaRetangulo}`);
+// //23
+// let cidadeUsuario = prompt('Digite o nome da sua cidade:');
+// cidadeUsuario = String(cidadeUsuario).toLowerCase().trim(); 
+// if (cidadeUsuario.startsWith('s')) {
+//     alert(`O nome da cidade "${cidadeUsuario}" começa com a letra 'S'.`);
+// } else {
+//     alert(`O nome da cidade "${cidadeUsuario}" não começa com a letra 'S'.`);
+// }
+// //24
+// const primeiroNumeroDecimal = Number(prompt('Digite o primeiro número decimal:'));
+// const segundoNumeroDecimal = Number(prompt('Digite o segundo número decimal:'));
+// console.log(`O resto da divisão dos números decimais é: ${(primeiroNumeroDecimal % segundoNumeroDecimal).toFixed(2)}`);
+// //25
+// const numeroDecimalArr = Number(prompt("Digite um número decimal a ser arredondado: "));
+// console.log(`O número ${numeroDecimalArr} arredondado é ${Math.floor(numeroDecimalArr)}`);
+// //26
+// const numeroInteiro = Number(prompt("Digite um número inteiro"));
+// const numeroInteiroAdicionado = numeroInteiro + 10;
+// const numeroString = numeroInteiroAdicionado.toString();
+// console.log(`${numeroString} é do tipo ${typeof numeroString}`);
+// //27
+// const dataNascUsuario = prompt("Digite sua data de nascimento. Exemplo: 'dd/mm/aaaa': ");
+// let dataNascSeparada: string[] = [];
+// if (dataNascUsuario != null) {
+//   dataNascSeparada = dataNascUsuario.split("/");
+// }
+// const diaNascimento: number = parseInt(dataNascSeparada[0]);
+// const mesNascimento: number = parseInt(dataNascSeparada[1]);
+// const anoNascimentoNum: number = parseInt(dataNascSeparada[2]);
+// console.log(`Dia: ${diaNascimento}, Mês: ${mesNascimento}, Ano: ${anoNascimentoNum}`);
+// //28
+// const estadoUsuario = prompt("Você já disse a cidade em que mora, agora diga qual o estado: ");
+// console.log(`Você mora em ${cidadeUsuario}, ${estadoUsuario}.`);
+// //29
+// console.log(`Bem-vindo ao nosso programa, nascido em ${dataNascUsuario}!`);
+// //30
+// const dadoInteiro = Number(prompt("Digite um número inteiro: "));
+// const dadoTexto = prompt("Digite uma string: ");
+// console.log(`Dados concatenados: ${dadoInteiro + " " + dadoTexto}`);
+// //31
+// const nomeProduto = prompt("Digite o produto: ");
+// const precoProduto = prompt("Digite o preço do produto: ");
+// console.log(`Resumo do pedido: ${nomeProduto}, R$${precoProduto}`);
+// //32
+// console.log(`O dobro de ${numeroInteiro} é igual a ${numeroInteiro ** 2}`);
+// //33
+// const emailUsuario = prompt("Digite seu e-mail: ");
+// console.log(`Obrigada por se inscrever. Continuaremos enviando notícias para o e-mail: ${emailUsuario}!!`);
+// //34
+// console.log(`A soma de ${numeroInteiro} + ${dadoInteiro} é ${numeroInteiro + dadoInteiro}`);
+// console.log(`A diferença de ${numeroInteiro} + ${dadoInteiro} é ${Math.abs(numeroInteiro - dadoInteiro)}`);
+// console.log(`O produto de ${numeroInteiro} + ${dadoInteiro} é ${numeroInteiro * dadoInteiro}`);
+// console.log(`O quociente de ${numeroInteiro} + ${dadoInteiro} é ${numeroInteiro / dadoInteiro}`);
+// //35
+// const baseTriangulo = Number(prompt("Digite a base do triângulo: "));
+// const alturaTriangulo = Number(prompt("Digite a altura do triângulo: "));
+// const areaTriangulo = baseTriangulo * alturaTriangulo;
+// console.log(`A base do triângulo é ${areaTriangulo}`);
+// //36
+// const raioCirculo = Number(prompt("Digite o raio de uma circunferência: "));
+// const piValor = 3.14;
+// const perimetroCirculo = 2 * piValor * raioCirculo;
+// console.log(perimetroCirculo);
+// //37
+// console.log(`O perímetro do triângulo 1 é: ${2 * (baseTriangulo + alturaTriangulo)}`);
+// //38
+// console.log(`A média aritmética entre os três números decimais já digitados aqui é: ${(numeroDecimal + primeiroNumeroDecimal + segundoNumeroDecimal / 3)}`);
+// //39
+// console.log(`${nomeUsuario}, você já viveu ${idadeCalculada * 365} dias e ${idadeCalculada * 12} meses.`);
+// //40
+// const valorRealUsuario = Number(prompt("Digite o valor em real: "));
+// const valorDolarAtual = Number(prompt("Digite a cotação do dólar atualmente: "));
+// const valorConvertidoDolar = valorRealUsuario * valorDolarAtual;
+// console.log(valorConvertidoDolar);
+// //41
+// console.log(`O número decimal ${numeroDecimalArr} convertido para o número mais próximo é: ${Math.ceil(numeroDecimalArr)}`);
+// //42
+// console.log(`O resultado da operação com três números inteiros ((n1 + n2) * n3) é: ${(numeroInteiro + dadoInteiro) * numeroInteiro}`);
+// //43
+// const temperaturaCelsius = Number(prompt("Digite uma temperatura em graus Celsius: "));
+// const temperaturaFahrenheit = (temperaturaCelsius * 9/5) + 32;
+// console.log(temperaturaFahrenheit);
